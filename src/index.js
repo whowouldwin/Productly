716233db49092bfa1fdfd8e2ae2f53873b4f8166
Lera Bessonova <leranetwork@gmail.com>
2023-02-09 18:29:58 +0300

    add function removeSelectedTags()

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('hello');
+window.onload = function () {
+    //tags
+    addTagsClickHandler();
+
+}
+const addTagsClickHandler = () => {
+    document.querySelector('.strategies__tags').addEventListener('click', (event) => {
+        if (event.target.classList.contains('tag')) {
+            let clickedTag = event.target;
+            removeSelectedTags();
+        }
+    })
+}
+const removeSelectedTags = () => {
+    let tags = document.querySelectorAll('.strategies__tags .tag');
+    console.log(tags);
+    tags.forEach(tag => {
+        tag.classList.remove('tag_selected');
+        tag.classList.add('tag_bordered');
+    })
+};
+
+
