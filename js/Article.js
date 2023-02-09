e8a2ab79e3e3c9edcfd59b6074154a1c33a5a101
Lera Bessonova <leranetwork@gmail.com>
2023-02-09 19:58:32 +0300

    add content in class Article

--- js/Article.js
+++ js/Article.js
@@ -1,3 +1,15 @@
 export class Article {
-    
+    constructor( {id, title, urlToImage, tags, ...rest} ) {
+        this.id = id;
+        this.title = title;
+        this.urlToImage = urlToImage;
+        this.tags = tags;
+    }
+    //Article generator
+    generateArticle() {
+        let template = '';
+        let article = document.createElement('article');
+        article.className = 'strategy block-shadowed';
+        article.setAttribute('data-id', this.id);
+    }
 }
