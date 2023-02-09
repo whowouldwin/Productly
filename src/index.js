c1a360eacf37b39a2b8e8daac128e7a63d34ca4f
Lera Bessonova <leranetwork@gmail.com>
2023-02-09 18:34:53 +0300

    add function selectClickedTag()

--- src/index.js
+++ src/index.js
@@ -7,17 +7,22 @@ const addTagsClickHandler = () => {
     document.querySelector('.strategies__tags').addEventListener('click', (event) => {
         if (event.target.classList.contains('tag')) {
             let clickedTag = event.target;
-            removeSelectedTags();
+            removeSelectedTag();
+            selectClickedTag(clickedTag);
         }
     })
 }
-const removeSelectedTags = () => {
+const removeSelectedTag = () => {
     let tags = document.querySelectorAll('.strategies__tags .tag');
     console.log(tags);
     tags.forEach(tag => {
         tag.classList.remove('tag_selected');
         tag.classList.add('tag_bordered');
     })
+};
+const selectClickedTag = (clickedTag) => {
+    clickedTag.classList.add('tag_selected');
+    clickedTag.classList.remove('tag_bordered');
 };
 
 
