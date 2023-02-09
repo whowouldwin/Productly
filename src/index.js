badc14f8020ef30cc7e78382e88e3ab0dccbde1b
Lera Bessonova <leranetwork@gmail.com>
2023-02-09 19:34:46 +0300

    add webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
+import {Article} from "../js/Article";
+
+const data = [
+    {
+        id: 1,
+        title: 'Increasing Prosperity With Positive Thinking',
+        urlToImage: './src/img/strategies/1.jpg',
+        tags: ['Art', 'Design'],
+        content: 'Knowing yourself is the first, and a very critical step in the process of planning your future. How can you figure out what you want to do with your life if you don’t know: What am I going to do with the  rest of my life? What is my dream job? What do I enjoy doing? What’s my passion? What kind of career fits my personality?',
+        date: '01.01.2020'
+    },
+    {
+        id: 2,
+        title: 'Motivation Is The First Step To Success',
+        urlToImage: './src/img/strategies/2.jpg',
+        tags: ['Culture'],
+        content: 'Knowing yourself is the first, and a very critical step in the process of planning your future. How can you figure out what you want to do with your life if you don’t know: What am I going to do with the  rest of my life? What is my dream job? What do I enjoy doing? What’s my passion? What kind of career fits my personality?',
+        date: '01.01.2020'
+    },
+    {
+        id: 3,
+        title: 'Success Steps For Your Personal Or Business Life',
+        urlToImage: './src/img/strategies/3.jpg',
+        tags: ['Culture', 'Design', 'Art'],
+        content: 'Knowing yourself is the first, and a very critical step in the process of planning your future. How can you figure out what you want to do with your life if you don’t know: What am I going to do with the  rest of my life? What is my dream job? What do I enjoy doing? What’s my passion? What kind of career fits my personality?',
+        date: '01.01.2020'
+    }
+];
+
+
 window.onload = function () {
     //tags
     addTagsClickHandler();
